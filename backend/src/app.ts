51c7d957f4b70317e01294c244d45f12e718a23e
3nd3r1 <ender.spam.guru@gmail.com>
2024-11-05 11:55:54 +0200

    refactor(backend): Add error handler middleware

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -2,6 +2,7 @@ import { Application } from "https://deno.land/x/oak@v17.1.2/mod.ts";
 
 import homeRouter from "./routes/home.ts";
 import authRouter from "./routes/auth.ts";
+import { errorHandler } from "./middlewares/error.ts";
 
 const app = new Application();
 
@@ -9,6 +10,7 @@ app.use(async (context, next) => {
     console.log(`${context.request.method} ${context.request.url}`);
     await next();
 });
+app.use(errorHandler);
 
 app.use(homeRouter.routes());
 app.use(authRouter.routes());
---
+++ backend/src/middlewares/error.ts
@@ -0,0 +1,19 @@
+import { Context, isHttpError, Next } from "https://deno.land/x/oak@v17.1.2/mod.ts";
+
+export const errorHandler = async (ctx: Context, next: Next) => {
+    try {
+        await next();
+    } catch (error: unknown) {
+        if (isHttpError(error)) {
+            ctx.response.status = error.status;
+        } else {
+            ctx.response.status = 500;
+        }
+
+        if (error instanceof Error) {
+            ctx.response.body = { error: error.message };
+        } else {
+            ctx.response.body = { error: "Something went wrong" };
+        }
+    }
+};
