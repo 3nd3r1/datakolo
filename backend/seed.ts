1344de4d0b8e48d07cbf19b694e2c8008b04a017
3nd3r1 <ender.spam.guru@gmail.com>
2025-01-06 16:40:00 +0200

    refactor(seed): Refactor the seed.ts in backend

--- backend/seed.ts
+++ backend/seed.ts
@@ -1,10 +1,6 @@
 import mongoose from "mongoose";
 
-import User from "@/models/user.ts";
-import Project from "@/models/project.ts";
-import { hashPassword } from "@/utils/hash.ts";
-import { NewProject } from "@/validators/project.ts";
-import { NewUser } from "@/validators/user.ts";
+import { seedDatabase } from "./tests/integration/db.ts";
 
 const connectDatabase = async () => {
     const databaseUrl = Deno.env.get("DATABASE_URL");
@@ -17,57 +13,9 @@ const connectDatabase = async () => {
     await mongoose.connect(databaseUrl);
 };
 
-const clearDatabase = async () => {
-    const collections = mongoose.connection.collections;
-    for (const key in collections) {
-        const collection = collections[key];
-        await collection.deleteMany({});
-    }
-};
-
-const seedUsers = async () => {
-    const seedUsers: NewUser[] = [
-        {
-            username: "John",
-            password: await hashPassword("password"),
-        },
-        {
-            username: "Jane",
-            password: await hashPassword("password"),
-        },
-    ];
-
-    console.log("Seeding users");
-    await User.insertMany(seedUsers);
-};
-
-const seedProjects = async () => {
-    const john = await User.findOne({ username: "John" });
-    if (!john) {
-        throw new Error("John user didn't exist");
-    }
-    const johnId = john._id.toString();
-
-    const seedProjects: NewProject[] = [
-        {
-            name: "Test Project 1",
-            createdBy: johnId,
-        },
-        {
-            name: "Test Project 2",
-            createdBy: johnId,
-        },
-    ];
-
-    console.log("Seeding projects");
-    await Project.insertMany(seedProjects);
-};
-
 async function seed() {
     await connectDatabase();
-    await clearDatabase();
-    await seedUsers();
-    await seedProjects();
+    await seedDatabase();
 
     console.log("Seeded successfully");
     await mongoose.disconnect();
