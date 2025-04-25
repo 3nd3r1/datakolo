1aaac2323271ba2f03a9f336ccb6b3c36dd4431c
3nd3r1 <ender.spam.guru@gmail.com>
2025-04-25 16:23:56 +0300

    fix(backend): Use port from env

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,6 +1,7 @@
 import app from "@/app.ts";
 import connectDatabase from "@/utils/db.ts";
+import { config } from "@/utils/config.ts";
 
 await connectDatabase();
 
-await app.listen({ port: 8000 });
+await app.listen({ port: config.server.port });
